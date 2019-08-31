d0e224ec72ba5d6f531ccd672811fb1d938c7e9f
chris <christianwilliamsjire@gmail.com>
2019-08-31 23:55:39 +0300

    this commit will add create user function

--- password.py
+++ password.py
@@ -1,1 +1,9 @@
 import pyperclip
+from credentials import User, Credential
+
+def create_user(fname,lname,password):
+    '''
+    Function to create a new user account
+    '''
+    new_user = User(fname,lname,password)
+    return new_user
