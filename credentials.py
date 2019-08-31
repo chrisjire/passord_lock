c7d4776ff3d6752314af0b9ae1bac6ce4adb87bd
chris <christianwilliamsjire@gmail.com>
2019-08-31 16:20:11 +0300

    this commit will will add class user to credentials.py

--- credentials.py
+++ credentials.py
@@ -2,3 +2,21 @@ import pyperclip
 import random
 import string
 
+#Global Variables
+global users_list
+class User:
+    '''
+    Class to create user accounts and save their information
+    '''
+    users_list = []
+    def __init__(self,first_name,last_name,age,password):
+        '''
+        Methord to define the properties for each user object
+        '''
+        
+        #Instance Variable
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.password = password
+        
