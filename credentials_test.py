6dc5832ddb45c8c5122ae12cefb221edde3458d4
chris <christianwilliamsjire@gmail.com>
2019-08-31 17:25:43 +0300

    this commit will add add test for save user function

--- credentials_test.py
+++ credentials_test.py
@@ -24,7 +24,12 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.age,'21')
         self.assertEqual(self.new_user.password,'Pwd123456')
         
-    
+    def test_save_user(self):
+        '''
+		Test to check if the new users info is saved into the users list
+		'''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
         
         
         
